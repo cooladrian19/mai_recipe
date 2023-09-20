343a6f54a222485a40ea1b3342be939b839468c3
cooladrian19 <adriannguyen8815@gmail.com>
2023-09-20 12:22:39 -0500

    Made improvments and added logIn page

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+import {initializeApp} from 'firebase/app' 
+import {getDatabase, ref, push } from 'firebase/database'
+
 const apiKey = "5d2491564ef64a7d8a7fcb77112b1327 ";
+const appSettings = {
+    databaseURL: "https://mai-s-recipe-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const userAccountsDB = ref(database, "userAccounts")
 
 function fetchRecipe (query) {
-    const url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=${query}&query=$(query}$number=10';
-
+    const url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=${query}&query=$(query}$number=10'
 }
+
