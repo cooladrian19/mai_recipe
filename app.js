67c499056797e20b73ef2a2f8a9eb25aec200e4a
cooladrian19 <adriannguyen8815@gmail.com>
2023-09-19 20:29:33 -0500

    Added files to repository

---
+++ app.js
@@ -0,0 +1,6 @@
+const apiKey = "5d2491564ef64a7d8a7fcb77112b1327 ";
+
+function fetchRecipe (query) {
+    const url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=${query}&query=$(query}$number=10';
+
+}
